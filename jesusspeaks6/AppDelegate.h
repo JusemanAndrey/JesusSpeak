3f9ef0dbdfbdee546a9a1455a1239b8c0b43d4c9
OkSeJu <okseju@OkSeJus-Mac.local>
2015-06-29 18:09:10 +0300

    Initial Commit

---
+++ jesusspeaks6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jesusspeaks6
+//
+//  Created by OkSeJu on 6/29/15.
+//  Copyright (c) 2015 fsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jesusspeaks6Tests/jesusspeaks6Tests.m
@@ -0,0 +1,40 @@
+//
+//  jesusspeaks6Tests.m
+//  jesusspeaks6Tests
+//
+//  Created by OkSeJu on 6/29/15.
+//  Copyright (c) 2015 fsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface jesusspeaks6Tests : XCTestCase
+
+@end
+
+@implementation jesusspeaks6Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
